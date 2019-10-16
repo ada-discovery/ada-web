c2f5a888c5d53e114452e60893a0d5c66612e62f
Peter Banda <tremor11@gmail.com>
2019-10-16 14:10:58 +0200

    Guice injector introduced in the csv import test.

--- test/unit/CsvImporterSpec.scala
+++ test/unit/CsvImporterSpec.scala
@@ -1,13 +1,13 @@
 package unit
 
+import com.google.inject.Injector
 import org.ada.server.models.dataimport.CsvDataSetImport
 import org.ada.server.services.ServiceTypes.DataSetCentralImporter
-import org.ada.server.services.importers.DataSetCentralImporterImpl
+import net.codingwell.scalaguice.InjectorExtensions._
+import org.ada.server.services.GuicePlayTestApp
 import org.scalatest._
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
-import play.api.inject.bind
-import play.api.inject.guice.GuiceApplicationBuilder
 
 import scala.io.Codec
 
@@ -17,21 +17,24 @@ class CsvImporterSpec extends FlatSpec with GuiceOneAppPerSuite {
   private val irisCsv = getClass.getResource("/iris.csv").toString
 
   override def fakeApplication(): Application =
-    new GuiceApplicationBuilder()
-      .bindings(bind[DataSetCentralImporter].to(classOf[DataSetCentralImporterImpl]))
-      .build()
+    GuicePlayTestApp()
+//    new GuiceApplicationBuilder()
+//      .bindings(bind[DataSetCentralImporter].to(classOf[DataSetCentralImporterImpl]))
+//      .build()
+
+  private def guiceInjector = app.injector.instanceOf[Injector]
 
   "CsvDataSetImport" should "run without error" in {
     val importInfo = CsvDataSetImport(
-      dataSpaceName = "",
-      dataSetId = "iris",
+      dataSpaceName = "test",
       dataSetName = "iris",
+      dataSetId = "test.iris",
       delimiter = ",",
       matchQuotes = false,
       inferFieldTypes = true,
       path = Some(irisCsv)
     )
-    val importer = app.injector.instanceOf[DataSetCentralImporter]
+    val importer = guiceInjector.instance[DataSetCentralImporter]
     importer(importInfo)
   }
 }
