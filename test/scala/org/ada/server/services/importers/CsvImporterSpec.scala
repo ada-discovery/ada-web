f8a46dc576f34b22188a017323ea8a45746c8524
Sascha Herzinger <sascha.herzinger@uni.lu>
2019-10-21 15:17:04 +0200

    added elasticsearch to test

--- test/scala/org/ada/server/services/importers/CsvImporterSpec.scala
+++ test/scala/org/ada/server/services/importers/CsvImporterSpec.scala
@@ -3,6 +3,7 @@ package scala.org.ada.server.services.importers
 import com.google.inject.Injector
 import net.codingwell.scalaguice.InjectorExtensions._
 import org.ada.server.dataaccess.dataset.DataSetAccessorFactory
+import org.ada.server.models.{DataSetSetting, StorageType}
 import org.ada.server.models.dataimport.CsvDataSetImport
 import org.ada.server.services.GuicePlayTestApp
 import org.ada.server.services.ServiceTypes.DataSetCentralImporter
@@ -14,30 +15,45 @@ class CsvImporterSpec extends AsyncFlatSpec {
 
   implicit override def executionContext = scala.concurrent.ExecutionContext.Implicits.global
   private implicit val codec = Codec.UTF8
-  private val irisCsv = getClass.getResource("/iris.csv").getPath
 
   private val guiceInjector = GuicePlayTestApp().injector.instanceOf[Injector]
   private val importer = guiceInjector.instance[DataSetCentralImporter]
   private val dsaf = guiceInjector.instance[DataSetAccessorFactory]
 
-  "CsvDataSetImport" should "import iris.csv to MongoDB" in {
-    val dataSetId = "test.iris"
-    val dataSetName = "iris"
-    val importInfo = CsvDataSetImport(
+  private object Iris {
+    val path = getClass.getResource("/iris.csv").getPath
+    val id = "test.iris"
+    val name = "iris"
+    val size = 150
+    def importInfo(storageType: StorageType) = CsvDataSetImport(
       dataSpaceName = "test",
-      dataSetName = dataSetName,
-      dataSetId = dataSetId,
+      dataSetName = name,
+      dataSetId = id,
       delimiter = ",",
       matchQuotes = false,
       inferFieldTypes = true,
-      path = Some(irisCsv)
+      path = Some(path),
+      setting = Some(new DataSetSetting(id, storageType))
     )
+  }
 
+  behavior of "CsvDataSetImport"
+
+  it should "import iris.csv to MongoDB" in {
     for {
-      _ <- importer(importInfo)
-      dsa = dsaf(dataSetId).getOrElse(fail(s"Dataset '$dataSetName' not found in DB."))
-      _ <- dsa.dataSetName map { name => assert(name == dataSetName) }
-      _ <- dsa.dataSetRepo.count() map { count => assert(count == 150)}
+      _ <- importer(Iris.importInfo(StorageType.Mongo))
+      dsa = dsaf(Iris.id).getOrElse(fail(s"Dataset '${Iris.name}' not found in Mongo."))
+      _ <- dsa.dataSetName map { name => assert(name == Iris.name) }
+      _ <- dsa.dataSetRepo.count() map { count => assert(count == Iris.size)}
+    } yield succeed
+  }
+
+  it should "import iris.csv to ElasticSearch" in {
+    for {
+      _ <- importer(Iris.importInfo(StorageType.ElasticSearch))
+      dsa = dsaf(Iris.id).getOrElse(fail(s"Dataset '${Iris.name}' not found in Elastic."))
+      _ <- dsa.dataSetName map { name => assert(name == Iris.name) }
+      _ <- dsa.dataSetRepo.count() map { count => assert(count == Iris.size)}
     } yield succeed
   }
 }
