4e9b5f2e55763e35e0a85eb554b653a980650e30
Sascha Herzinger <sascha.herzinger@uni.lu>
2019-10-22 15:10:28 +0200

    added JsonDataSetImporterSpec

---
+++ test/scala/org/ada/server/services/importers/JsonDataSetImporterSpec.scala
@@ -0,0 +1,62 @@
+package scala.org.ada.server.services.importers
+
+import com.google.inject.Injector
+import net.codingwell.scalaguice.InjectorExtensions._
+import org.ada.server.dataaccess.dataset.DataSetAccessorFactory
+import org.ada.server.models.{DataSetSetting, StorageType}
+import org.ada.server.models.dataimport.CsvDataSetImport
+import org.ada.server.services.GuicePlayTestApp
+import org.ada.server.services.ServiceTypes.DataSetCentralImporter
+import org.scalatest._
+
+class JsonDataSetImporterSpec extends AsyncFlatSpec with BeforeAndAfter {
+
+  implicit override def executionContext = scala.concurrent.ExecutionContext.Implicits.global
+
+  private val guiceInjector = GuicePlayTestApp().injector.instanceOf[Injector]
+  private val importer = guiceInjector.instance[DataSetCentralImporter]
+  private val dsaf = guiceInjector.instance[DataSetAccessorFactory]
+
+  private object Iris {
+    val path = getClass.getResource("/iris.json").getPath
+    val id = "test.iris"
+    val name = "iris"
+    val size = 150
+    def importInfo(storageType: StorageType.Value) = CsvDataSetImport(
+      dataSpaceName = "test",
+      dataSetName = name,
+      dataSetId = id,
+      delimiter = ",",
+      matchQuotes = false,
+      inferFieldTypes = true,
+      path = Some(path),
+      setting = Some(new DataSetSetting(id, storageType))
+    )
+  }
+
+  after {
+    dsaf(Iris.id) map { _.dataSetRepo.deleteAll }
+  }
+
+  behavior of "JsonDataSetImporter"
+
+  it should "import iris.json to MongoDB" in {
+    for {
+      _ <- importer(Iris.importInfo(StorageType.Mongo))
+      dsa = dsaf(Iris.id).getOrElse(fail(s"Dataset '${Iris.name}' not found in Mongo."))
+      _ <- dsa.dataSetRepo.flushOps
+      _ <- dsa.dataSetName map { name => assert(name == Iris.name) }
+      _ <- dsa.dataSetRepo.count() map { count => assert(count == Iris.size)}
+    } yield succeed
+  }
+
+  it should "import iris.json to ElasticSearch" in {
+    for {
+      _ <- importer(Iris.importInfo(StorageType.ElasticSearch))
+      dsa = dsaf(Iris.id).getOrElse(fail(s"Dataset '${Iris.name}' not found in Elastic."))
+      _ <- dsa.dataSetRepo.flushOps
+      _ <- dsa.dataSetName map { name => assert(name == Iris.name) }
+      _ <- dsa.dataSetRepo.count() map { count => assert(count == Iris.size)}
+    } yield succeed
+  }
+}
